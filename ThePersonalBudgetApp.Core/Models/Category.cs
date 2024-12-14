0210e770c6a05eb6626e8dd286db06310dc73f76
Gr2dd3 <gradde23@gmail.com>
2024-12-14 14:40:48 +0100

    first init

---
+++ ThePersonalBudgetApp.Core/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ThePersonalBudgetApp.Core.Models;
+
+internal class Category
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public float TotalAmount { get; set; } = 0;
+    public List<Item> Items { get; set; } = new List<Item>();
+}
---
+++ ThePersonalBudgetApp.Core/Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace ThePersonalBudgetApp.Core.Models;
+
+internal class Item
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public float Amount { get; set; }
+}
