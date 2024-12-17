bab186acb3d713a30ed4239837dd5a3fd393150a
Gr2dd3 <gradde23@gmail.com>
2024-12-17 12:51:35 +0100

    added a budgetManager with DI and start of first Method

---
+++ ThePersonalBudgetApp/DAL/Managers/BudgetManager.cs
@@ -0,0 +1,17 @@
+﻿using ThePersonalBudgetApp.Context;
+
+namespace ThePersonalBudgetApp.DAL.Managers;
+
+public class BudgetManager
+{
+    BudgetDbContext _context;
+    public BudgetManager(BudgetDbContext context)
+    {
+        _context = context;
+    }
+
+    public void SaveBudget()
+    {
+
+    }
+}
